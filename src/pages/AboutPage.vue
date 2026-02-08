352d2852680804599de42b4ddfc317f22ca6de3a
francescodelgiudice <fra.delgiu16@gmail.com>
2026-02-08 10:16:00 +0100

    i18n: new about text

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,7 +1,27 @@
 <template>
   <q-page class="flex justify-center full-width">
     <div class="container">
-      <card-wrapper> Ciao! </card-wrapper>
+      <card-wrapper>
+        <div>
+          Ciao! <br />
+          Ho creato questo sito per replicare
+          <a
+            href="https://www.minutecryptic.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="bg-primary font-bold"
+          >
+            Minute Cryptic
+          </a>
+          per il compleanno di Emma e San Valentino. <br />
+          Se stai leggendo questo testo e non sei Emma, buon gioco e fatti gli affari tuoi :) <br />
+          Se invece sei Emma, spero che questo sito ti piaccia e che ti faccia divertire! <br />
+          È bellissimo dopo 10 anni continuare a trovare qualcosa di nuovo da condividere e da
+          scoprire insieme. <br />
+          Ti amo <br />
+          <span class="bg-primary font-bold">Fra</span>
+        </div>
+      </card-wrapper>
     </div>
   </q-page>
 </template>
